9cf3109ee885c75eaf36f87f96e1eeb997fa8621
Stallixx <iamstallixx@gmail.com>
2018-04-12 02:06:40 +0800

    Update to v3.4.1

--- config.sh
+++ config.sh
@@ -59,7 +59,7 @@ print_modname() {
   ui_print "    *******************************************"
   ui_print "    *            Pixel 2 Experience           *"
   ui_print "    *******************************************"
-  ui_print "    *                  v3.4                   *"
+  ui_print "    *                  v3.4.1                 *"
   ui_print "    *                Joey Huab                *"
   ui_print "    *******************************************"
   ui_print " "
