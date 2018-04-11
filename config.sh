3ab256b9bc752de7ce478b21c1909c765ff83c90
Stallixx <iamstallixx@gmail.com>
2018-04-12 01:09:40 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -85,6 +85,9 @@ REPLACE="
 # Construct your own list here, it will overwrite the example
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
+/system/priv-app/Launcher3
+/system/priv-app/NexusLauncherPrebuilt
+/system/priv-app/Trebuchet
 "
 
 ##########################################################################################
